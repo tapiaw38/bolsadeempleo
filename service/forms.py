9554ac9a269e19d7d4df8e686a948fefe25e51f4
tapiaw38 <tapiaw38@gmail.com>
2020-06-23 12:25:14 -0300

    choices

--- service/forms.py
+++ service/forms.py
@@ -7,4 +7,37 @@ class ServiceForm(forms.ModelForm):
     class Meta:
 
         model = Service
-        fields = ('user', 'person', 'title', 'category', 'description', 'direction', 'facebook_url', 'picture_logo')
+
+        fields = (
+            'user', 
+            'person', 
+            'title', 
+            'category', 
+            'description', 
+            'direction', 
+            'facebook_url', 
+            'picture_logo'
+            )
+        
+        CATEGORY_CHOICES = [
+            ('Compras y Shopping', 'Compras y Shopping'),
+            ('Informática y Electrónica','Informática y Electrónica'),
+            ('Construcción y Contratistas','Construcción y Contratistas'),
+            ('Deportes y Recreacción','Deportes y Recreacción'),
+            ('Educación','Educación'),
+            ('Hogar y Jardin','Hogar y Jardin'),
+            ('Industria y Agricultura','Industria y Agricultura'),
+            ('Medios de Comunicación','Medios de Comunicación'),
+            ('Servicios Profecionales','Servicios Profecionales'),
+            ('Ropa y Accesorios','Ropa y Accesorios'),
+            ('Salud y Medicina','Salud y Medicina'),
+            ('Servicios Legales y Financieros','Servicios Legales y Financieros'),
+            ('Viajes y Trasportes','Viajes y Trasportes'),
+            ('Gastronomia','Gastronomia'),
+        ]
+
+
+
+        widgets = {
+            'category':forms.Select(choices=CATEGORY_CHOICES,attrs={'class': 'form-control'}),
+        }
