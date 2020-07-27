cf063b696dd9ede80d1a5b0f8ecb57eab111f7a0
tapia <tapiaw38@gmail.com>
2020-07-27 01:28:16 -0300

    cambio-tamaño-img-user

--- user/models.py
+++ user/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+# Import image
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import sys
 # Create your models here.
 
 class Person(models.Model):
@@ -10,6 +15,28 @@ class Person(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
+    def save(self, *args, **kwargs):
+        imageTemproary = Image.open(self.picture)
+        imageTemproary = imageTemproary.convert('RGB')
+        outputIoStream = BytesIO()
+
+        w, h = imageTemproary.size
+
+        if w > 2000 and h > 1400:
+            w = int(w/3)
+            h = int(h/3)
+        elif w > 1000 and h > 750:
+            w = int(w/2)
+            h = int(h/2)
+
+
+
+        imageTemproaryResized = imageTemproary.resize((w,h)) 
+
+        imageTemproaryResized.save(outputIoStream , format='JPEG', quality=150)
+        outputIoStream.seek(0)
+        self.picture = InMemoryUploadedFile(outputIoStream,'ImageField', "%s.jpg" %self.picture.name.split('.')[0], 'image/jpeg', sys.getsizeof(outputIoStream), None)
+        super(Person, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.user.username
