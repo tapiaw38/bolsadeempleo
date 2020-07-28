5e039ae896ef9616973b15a67aa118498282bccc
tapiaw38 <tapiaw38@gmail.com>
2020-07-28 11:06:48 -0300

    pasar-login

--- bolsadeempleo/middleware.py
+++ bolsadeempleo/middleware.py
@@ -12,7 +12,8 @@ class PersonCompletationMiddleware:
         if not request.user.is_anonymous:
             if not request.user.is_staff:
                 person = request.user.person
-                if not person.picture or not person.phone_number or not person.direction:
+                #if not person.picture or not person.phone_number or not person.direction:
+                if not person.phone_number:
                     if request.path not in [reverse('update_person'), reverse('log_out')]:
                         return redirect('update_person')
             
